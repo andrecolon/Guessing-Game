97db0cad766f899adeb0e9eb6275edf431623e58
Andre Colon <andrecolon@gmail.com>
2019-06-13 20:49:17 -0700

    Guessing Game This will work as both testing Android to Git. And updating working code.
    0.0.4
    Added OnClick Listeners -- By the way inteli-sense did not autopop the code.
    
    Added Log to print out inputted text
    Added Integer parer for when the guessing of an integer is ready
    Added a lot - Else IF conditions for checkGuess
    
    Issues -->> Unbalanced Brace..working it out

--- app/src/main/java/com/vidordesigns/guessinggame/MainActivity.java
+++ app/src/main/java/com/vidordesigns/guessinggame/MainActivity.java
@@ -8,19 +8,24 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import static android.view.View.*;
 
 
 public class MainActivity extends AppCompatActivity {
+    // Class as a whole - Open variable for all classes
+        static final int PERFECT = 42;
+        static final int CORRECT = 0;
+        static final int OVER = 1;
+        static final int UNDER = 2;
+        static final int RESET = 3;
 
-//    static final int CORRECT = 0;
-//    static final int LOW = 1;
-//    static final int HIGH = 2;
-//    static final int RESET = 3;
-
+// Variables - not sure if that is what java is calling them
         EditText editDemo;
         Button buttonDemo;
+        TextView askQuestion, textResult, noteIssues; // I like this Idea
+        int guess;
 
 
     @Override
@@ -28,19 +33,57 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editDemo = findViewById(R.id.edit_demo);
-        buttonDemo = findViewById(R.id.button_demo);
-        buttonDemo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view){
-                String content = String.valueOf(editDemo.getText());
-                Log.i("Demo",content);
-                //int nuym = content * 6;
-                int value = Integer.parseInt(content);
-                //nuym = alue *2;
+        //Attach my variables to the UI by id
+        editDemo    = findViewById(R.id.edit_demo);
+        buttonDemo  = findViewById(R.id.button_demo);
+        askQuestion = findViewById(R.id.ask_question);
+        textResult  = findViewById(R.id.text_result);
+        noteIssues  = findViewById(R.id.note_issues);
 
+        buttonDemo.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view){
+
+                // get the numerical guess parse number and pass it through guess variable
+                guess = Integer.parseInt(askQuestion.getText().toString());
+                // Go through argument for chckGss in the integer that was passed in
+                int result = checkGuess(guess);
+
+                updateUI(result);
 
              }
         });
+}
+    int checkGuess(int guess) {
+        //check to see if the number is correct and return the result as an integer value
+
+        if (guess == PERFECT) {
+            return CORRECT;
+        } else if (guess < PERFECT) {
+            return UNDER;
+        } else {
+            return OVER;
+        }
+    }
+
+        public static void updateUI(int results) {
+
+
+            switch (results) {
+                case CORRECT:
+                    results.setText("The number is " + CORRECT);
+                    break;
+                case OVER:
+                    results.setText("Too high, think the meaning of life");
+                    break;
+                case UNDER:
+                    results.setText("Too low, think of my age when starting this venture");
+                    break;
+                default:
+                    results.setText("what's that numba again?");
+
+            }
+        }
+
     }
 }
